4a37df2e447f0b472841272d3697f6a9cc63b4df
SpockBauru <67930504+SpockBauru@users.noreply.github.com>
2022-02-10 23:31:22 -0300

    Added IO_DisableTextScrolling

---
+++ IO_DisableTextScrolling/IO_DisableTextScrolling.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+
+namespace IO_DisableTextScrolling
+{
+    [BepInProcess("IO")]
+    [BepInPlugin("spockbauru.insultorder.io_DisableTextScrolling", "IO_DisableTextScrolling", Version)]
+    public class IO_DisableTextScrolling : BaseUnityPlugin
+    {
+        // Set version in BepInEx and in AssemblyInfo
+        public const string Version = "1.0";
+
+        // User Configurations
+        private static ConfigEntry<bool> Enabled;
+
+        public IO_DisableTextScrolling()
+        {
+            // Config Panel Settings
+            Enabled = Config.Bind("General", "Enabled", true, "Whether the plugin is enabled");
+
+            // Patch Everything
+            Harmony.CreateAndPatchAll(typeof(IO_DisableTextScrolling));
+        }
+
+        //===============================================Hooks===============================================
+        //Disabling flag for text scrolling
+        [HarmonyPostfix, HarmonyPatch(typeof(ADV_Loader), "LineLoad")]
+        private static void DisableScrollling(string ___Text, ref int ___m_textPos)
+        {
+            if (Enabled.Value)
+                ___m_textPos = ___Text.Length;
+        }
+    }
+}
