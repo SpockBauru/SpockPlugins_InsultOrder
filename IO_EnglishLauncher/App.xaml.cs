69864a56070cbb9e8317a248304125a62948ac2d
SpockBauru <67930504+SpockBauru@users.noreply.github.com>
2022-02-16 09:30:10 -0300

    IO_EnglishLauncher v1.0

--- IO_EnglishLauncher/App.xaml.cs
+++ IO_EnglishLauncher/App.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
+using System.Threading;
 
 namespace IO_EnglishLauncher
 {
@@ -12,5 +8,28 @@ namespace IO_EnglishLauncher
     /// </summary>
     public partial class App : Application
     {
-    }
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			if (!mutex.WaitOne(0, false))
+			{
+				MessageBox.Show("Miconisomi is already running.", "Miconisomi", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+				mutex.Close();
+				mutex = null;
+				base.Shutdown();
+			}
+		}
+
+		// Token: 0x06000009 RID: 9 RVA: 0x000020F4 File Offset: 0x000002F4
+		private void Application_Exit(object sender, ExitEventArgs e)
+		{
+			if (mutex != null)
+			{
+				mutex.ReleaseMutex();
+				mutex.Close();
+			}
+		}
+
+		// Token: 0x04000004 RID: 4
+		private Mutex mutex = new Mutex(false, "Miconisomi");
+	}
 }
--- IO_EnglishLauncher/Properties/AssemblyInfo.cs
+++ IO_EnglishLauncher/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IO_EnglishLauncher")]
+[assembly: AssemblyTitle("Insult Order English Launcher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IO_EnglishLauncher")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCompany("Miconisomi")]
+[assembly: AssemblyProduct("Insult Order English Launcher")]
+[assembly: AssemblyCopyright("Miconisomi")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
