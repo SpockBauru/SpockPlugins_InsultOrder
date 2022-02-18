8c0e9d86ce91c4f25d0611c02d3615569f5fc701
SpockBauru <67930504+SpockBauru@users.noreply.github.com>
2022-02-18 16:59:59 -0300

    Added MirrorResolution

---
+++ IO_MirrorResolution/IO_MirrorResolution.cs
@@ -0,0 +1,60 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System.ComponentModel;
+
+namespace IO_MirrorResolution
+{
+    [BepInProcess("IO")]
+    [BepInPlugin("spockbauru.insultorder.io_BetterMiorror", "IO_MirrorResolution", Version)]
+    public class IO_MirrorResolution : BaseUnityPlugin
+    {
+        // Set version in BepInEx and in AssemblyInfo
+        public const string Version = "1.0";
+
+        // User Configurations
+        private static ConfigEntry<MirrorRes> Resolutions;
+
+        public IO_MirrorResolution()
+        {
+            // Config Panel Settings
+            Resolutions = Config.Bind("Settings", "Mirror Resolution", MirrorRes.r1024, "Set according your screen resolution. May be heavy on both GPU and vRAM");
+
+            // Patch Everything
+            Harmony.CreateAndPatchAll(typeof(IO_MirrorResolution));
+        }
+
+        //=======================================Configuration Manager=======================================
+        // User can define in which place he wants to play the animation
+        private enum MirrorRes
+        {
+            [Description("8x8: Better disable mirror in settings...")]
+            r8 = 8,
+            [Description("16x16: But why?")]
+            r16 = 16,
+            [Description("32x32: Big Blurs")]
+            r32 = 32,
+            [Description("64x64: Mosaic Glass")]
+            r64 = 64,
+            [Description("128x128: Potato PC")]
+            r128 = 128,
+            [Description("256x256: Game Defaults")]
+            r256 = 256,
+            [Description("512x512: HD resolutions")]
+            r512 = 512,
+            [Description("1024x1024: FullHD Resolution")]
+            r1024 = 1024,
+            [Description("2048x2048: 4k Resolution")]
+            r2048 = 2048,
+            [Description("4096x4096: Are you really sure?")]
+            r4096 = 4096
+        }
+
+        //======================================= Hooks =======================================
+        [HarmonyPrefix, HarmonyPatch(typeof(MirrorReflection), "CreateMirrorObjects")]
+        public static void SetMirrorResolution(ref int ___m_TextureSize)
+        {
+            ___m_TextureSize = (int)Resolutions.Value;
+        }
+    }
+}
---
+++ IO_MirrorResolution/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IO_MirrorResolution")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IO_MirrorResolution")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5795be28-0025-4d88-8c81-e3525bd885bd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(IO_MirrorResolution.IO_MirrorResolution.Version)]
+[assembly: AssemblyFileVersion(IO_MirrorResolution.IO_MirrorResolution.Version)]
